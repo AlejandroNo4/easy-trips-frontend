309a4f6dd92f3d917da7e09a11df8266ddb638be
Alejandro Contreras <kdta15@outlook.com>
2021-09-24 13:51:32 -0500

    Install object-to-formdata. Get rid of the comments and useless code.

--- src/components/CreateAccount.js
+++ src/components/CreateAccount.js
@@ -1,44 +1,30 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { Redirect } from 'react-router';
-import FormSignUp from './FormSignUp';
+import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router";
+import { serialize } from "object-to-formdata";
+import FormSignUp from "./FormSignUp";
 
 const CreateAccount = () => {
   const initialStateForm = {
-    username: '',
-    email: '',
-    password: '',
-    passwordConfirmation: '',
+    username: "",
+    email: "",
+    password: "",
+    passwordConfirmation: "",
     image: [],
   };
   const [form, updateInput] = useState(initialStateForm);
 
   const handleChange = (e) => {
-    if (e.target.files) {
-      // const reader = new FileReader();
-      // reader.readAsDataURL(e.target.files[0]);
-
-      // reader.onload = () => {
-      updateInput({ ...form, [e.target.name]: URL.createObjectURL(e.target.files[0]) });
-      // };
-    } else {
-      updateInput({ ...form, [e.target.name]: e.target.value });
-    }
+    if (e.target.files)
+      updateInput({ ...form, [e.target.name]: e.target.files[0] });
+    else updateInput({ ...form, [e.target.name]: e.target.value });
   };
 
   console.log(form);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const {
-      username, email, password, passwordConfirmation, image,
-    } = form;
-    // const fd = new FormData();
-    // fd.append("user[0]['username']", username);
-    // fd.append("user[0]['email']", email);
-    // fd.append("user[0]['password']", password);
-    // fd.append("user[0]['password_confirmation']", passwordConfirmation);
-    // fd.append("user[0]['image']", image);
+    const { username, email, password, passwordConfirmation, image } = form;
 
     const dataUserCreate = {
       user: {
@@ -50,15 +36,17 @@ const CreateAccount = () => {
       },
     };
 
+    const formData = serialize(dataUserCreate);
+
     try {
-      const request = await axios({
-        method: 'post',
-        url: 'https://alleasytrips.herokuapp.com/api/v1/users',
-        data: dataUserCreate,
-        withCredentials: true,
-      });
+      console.log(formData);
+      const request = await axios.post(
+        "https://alleasytrips.herokuapp.com/api/v1/users",
+        formData,
+        { withCredentials: true }
+      );
       console.log(request);
-        <Redirect to="/" />;
+      <Redirect to="/" />;
     } catch (error) {
       console.log(error.message);
     }
